e2e48e18ec9b44a7e1a2edf0d1f6e4052d05055c
S.W.A.T.  O.  P.L.U.S <swatoplus@gmail.com>
2018-05-27 14:50:04 +0300

    Добавьте файлы проекта.

---
+++ PaySystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PaySystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
